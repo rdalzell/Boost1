27b59f77d8d740b2ce9babd98aba0ea107447d8f
rdalzell <rdalzell@alertlog.com>
2017-12-17 16:23:53 +0000

    next

---
+++ Assets/rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour {
+
+    Rigidbody rigitbody;
+
+	// Use this for initialization
+	void Start () {
+        rigitbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessKey();
+	}
+
+    private void ProcessKey()
+    {
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            rigitbody.AddRelativeForce(Vector3.up);    
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Port Thrust");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Starboard Thrust");
+        }    
+                
+    }
+}
